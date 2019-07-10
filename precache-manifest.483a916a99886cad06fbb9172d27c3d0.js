6e0ea6f4fafcc9e0a5b8774080b18a61fafd8804
ranSharon <ransh360@gmail.com>
2019-07-10 20:09:36 +0300

    Updates

---
+++ precache-manifest.483a916a99886cad06fbb9172d27c3d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57e8f0ca6d9c4def4c24856e177fb68",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "87b1e457023881a24b5d",
+    "url": "/Quiz-App/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "e417313a739b3228f3b7",
+    "url": "/Quiz-App/static/js/2.8e0757a5.chunk.js"
+  },
+  {
+    "revision": "87b1e457023881a24b5d",
+    "url": "/Quiz-App/static/js/main.820cabdd.chunk.js"
+  },
+  {
+    "revision": "c30db239273d780c0bee",
+    "url": "/Quiz-App/static/js/runtime~main.9f56f2ee.js"
+  },
+  {
+    "revision": "688770deab00da50d4be34d7b94d8839",
+    "url": "/Quiz-App/static/media/friends.688770de.jpg"
+  },
+  {
+    "revision": "2432dfb3ae8201da7fa1f87aa083ca59",
+    "url": "/Quiz-App/static/media/got.2432dfb3.jpg"
+  },
+  {
+    "revision": "b768c2be90c9d3e99003d24ccbd3367f",
+    "url": "/Quiz-App/static/media/sv.b768c2be.png"
+  }
+]);
